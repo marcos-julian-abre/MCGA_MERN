f7726797fe4a6bd4e2febab0f25832572e255df3
Marcos Abre <marcosabre15@gmail.com>
2023-04-14 07:41:33 -0300

    Controladores de metas

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler (async (req, res) => {
+    res.status(200).json({ message: 'Get Goals'})
+})
+
+const setGoals = asyncHandler (async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error("Agrega campo texto")
+    }
+    
+    res.status(200).json({ message: 'Set Goals'})
+})
+
+const updateGoal = asyncHandler (async (req, res) => {
+    res.status(200).json({message: `Update Goal ${req.params.id}`})
+})
+
+const deleteGoal = asyncHandler (async (req, res) => {
+    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+})
+
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoal,
+    deleteGoal,
+}
