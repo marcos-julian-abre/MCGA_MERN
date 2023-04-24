9fc55af70c453c0cc9ec51e8e05ccce064bb737f
Marcos Abre <marcosabre15@gmail.com>
2023-04-23 21:05:02 -0300

    black magic

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,33 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { reset, logout } from '../features/auth/authSlice'
 
 
 function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () =>{
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className='header'>
         <div className='logo'>
             <Link to='/'>Metas</Link>
         </div>
         <ul>
+            {user ? (
             <li>
+                <button className='btn' onClick={onLogout}> 
+                <FaSignOutAlt/> Salir
+                </button>
+            </li>) :(<>
+             <li>
                 <Link to='/login'> 
                 <FaSignInAlt/> Ingresar
                 </Link>
@@ -18,7 +36,8 @@ function Header() {
                 <Link to='/register'> 
                 <FaUser/> Registrarse
                 </Link>
-            </li>
+            </li></>) }
+           
         </ul>
     </header>
 
