2f7e4002115bf6b3f35f94223bea9da1b9da4a13
Marcos Abre <marcosabre15@gmail.com>
2023-04-25 22:42:35 -0300

    funcionalidad metas

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -32,14 +32,12 @@ const updateGoal = asyncHandler (async (req, res) => {
         throw new Error ('Falta Meta')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('Usuario No Encontrado')
     }
 
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('Usuario No Autorizado')
     }
@@ -57,14 +55,12 @@ const deleteGoal = asyncHandler (async (req, res) => {
         throw new Error ('Falta Meta')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('Usuario No Encontrado')
     }
 
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('Usuario No Autorizado')
     }
